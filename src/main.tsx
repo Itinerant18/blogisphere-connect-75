24eefab19595091f228c0b02fd2bfe47c783ae7b
Aniket karmakar <94900936+Itinerant18@users.noreply.github.com>
2025-02-28 10:28:37 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
 
+// TODO: For production deployment, replace this with a production key from Clerk
+// The warning "Clerk has been loaded with development keys" will disappear when using production keys
 const PUBLISHABLE_KEY = "pk_test_ZmFuY3ktZWFnbGUtNi5jbGVyay5hY2NvdW50cy5kZXYk";
 
 if (!PUBLISHABLE_KEY) {
